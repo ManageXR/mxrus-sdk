f979ac7136d11f1ffe56b340682b6eb7a95dc67e
Vatsal Ambastha <ambastha.vatsal@gmail.com>
2025-04-09 06:54:26 +0530

    Logo markers can now have default logos or make themselves invisible
    Texture2D ILogoMarker.DefaultLogo allows setting a default logo to logo markers.
    
    If setting logo to null:
    - If a default logo is present, it is shown
    - Otherwise the marker is hidden

--- Embeddings/Markers/MonoImageLogoMarker.cs
+++ Embeddings/Markers/MonoImageLogoMarker.cs
@@ -8,17 +8,34 @@ namespace MXRUS.SDK {
     [RequireComponent(typeof(Image))]
     public class MonoImageLogoMarker : MonoBehaviour, ILogoMarker {
         [SerializeField] LogoMarkerType _logoMarkerType;
+        [SerializeField] Sprite _defaultLogo;
 
         public LogoMarkerType LogoMarkerType => _logoMarkerType;
+
+        public Texture2D DefaultLogo => _defaultLogo == null ? null : _defaultLogo.texture;
 
         private Image _image;
 
         private void Awake() {
             _image = GetComponent<Image>();
+            SetLogo(DefaultLogo);
         }
 
         public void SetLogo(Texture2D texture) {
-            _image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.one / 2);
+            if (texture != null) {
+                _image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.one / 2);
+                _image.color = Color.white;
+            }
+            else {
+                if (DefaultLogo != null) {
+                    _image.sprite = Sprite.Create(DefaultLogo, new Rect(0, 0, DefaultLogo.width, DefaultLogo.height), Vector2.one / 2);
+                    _image.color = Color.white;
+                }
+                else {
+                    _image.sprite = null;
+                    _image.color = Color.clear;
+                }
+            }
         }
     }
 }
